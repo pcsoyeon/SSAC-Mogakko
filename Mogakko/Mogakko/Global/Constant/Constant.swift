4d83db7b5febf8305c79a1fb8fa8e2d6332e8bf3
so yeon <alicesysy@swu.ac.kr>
2022-11-07 16:36:46 +0900

    [#1] Image 파일 추가 및 Image 상수화

---
+++ Mogakko/Mogakko/Global/Constant/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  Mogakko
+//
+//  Created by 소연 on 2022/11/07.
+//
+
+import Foundation
+
+enum Constant {
+    
+}
---
+++ Mogakko/Mogakko/Global/Constant/Image.swift
@@ -0,0 +1,59 @@
+//
+//  UIImage.swift
+//  Mogakko
+//
+//  Created by 소연 on 2022/11/07.
+//
+
+import UIKit
+
+extension Constant {
+    struct Image {
+        
+        // MARK: - Icon
+        
+        let antenna = UIImage(named: "antenna")!
+        let arrow = UIImage(named: "arrow")!
+        let bell = UIImage(named: "bell")!
+        let cancelMatch = UIImage(named: "cancel_match")!
+        let check = UIImage(named: "check")!
+        let closeBig = UIImage(named: "close_big")!
+        let closeSmall = UIImage(named: "close_small")!
+        let faq = UIImage(named: "faq")!
+        let filterControl = UIImage(named: "filter_control")!
+        let friendsPlus = UIImage(named: "friends_plus")!
+        let logout = UIImage(named: "logout")!
+        let man = UIImage(named: "man")!
+        let message = UIImage(named: "message")!
+        let moreArrow = UIImage(named: "more_arrow")!
+        let more = UIImage(named: "more")!
+        let notice = UIImage(named: "notice")!
+        let permit = UIImage(named: "permit")!
+        let place = UIImage(named: "place")!
+        let plus = UIImage(named: "plus")!
+        let qna = UIImage(named: "qna")!
+        let search = UIImage(named: "search")!
+        let settingAlarm = UIImage(named: "setting_alarm")!
+        let siren = UIImage(named: "siren")!
+        let woman = UIImage(named: "woman")!
+        let wirte = UIImage(named: "write")!
+        
+        // MARK: - Image
+        
+        let badge = UIImage(named: "badge")!
+        
+        // Tap 
+        let friendsAct = UIImage(named: "friendsAct")!
+        let friendsInact = UIImage(named: "friendsInact")!
+        let homeAct = UIImage(named: "homeAct")!
+        let homeInact = UIImage(named: "homeInact")!
+        let img = UIImage(named: "img")!
+        let mapMarker = UIImage(named: "map_marker")!
+        let myAct = UIImage(named: "myAct")!
+        let myInact = UIImage(named: "myInact")!
+        let sendAct = UIImage(named: "sendAct")!
+        let sendInact = UIImage(named: "sendInact")!
+        let shopAct = UIImage(named: "shopAct")!
+        let shopInact = UIImage(named: "shopInact")!
+    }
+}
