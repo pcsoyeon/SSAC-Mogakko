b373260a625d95f0a3505cdb1cc05eac8108ebd1
so yeon <alicesysy@swu.ac.kr>
2022-11-07 15:36:45 +0900

    [#1] Base Protocol 추가

---
+++ Mogakko/Mogakko/Global/Protocol/BaseViewControllerAttribute.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  Mogakko
+//
+//  Created by 소연 on 2022/11/07.
+//
+
+import Foundation
+
+protocol BaseViewControllerAttribute {
+    func configureHierarchy()
+    func setAttribute()
+    func bind()
+}
---
+++ Mogakko/Mogakko/Global/Protocol/Reusable.swift
@@ -0,0 +1,30 @@
+//
+//  Reusable.swift
+//  Mogakko
+//
+//  Created by 소연 on 2022/11/07.
+//
+
+import UIKit
+
+public protocol Reusable {
+    static var reuseIdentifier: String { get }
+}
+
+extension UIViewController: Reusable {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: Reusable {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: Reusable {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
