0f5ffe36620ba127fc275a6c6aecbae4a0b64809
so yeon <alicesysy@swu.ac.kr>
2022-11-08 09:05:23 +0900

    [#4] 핸드폰 번호 정규식 extension 추가

--- Mogakko/Mogakko/Global/Extension/String+Extension.swift
+++ Mogakko/Mogakko/Global/Extension/String+Extension.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension String {
+    func toPhoneNumberPattern(pattern: String, replacmentCharacter: Character) -> String {
+        var pureNumber = self.replacingOccurrences( of: "[^0-9]", with: "", options: .regularExpression)
+        pureNumber = pureNumber.count > 11 ? String(pureNumber[...pureNumber.index(startIndex,offsetBy: 10)]) : pureNumber
+        
+        for index in 0..<pattern.count {
+            guard index < pureNumber.count else { return pureNumber }
+            
+            let stringIndex = String.Index(utf16Offset: index, in: pattern)
+            let patternCharacter = pattern[stringIndex]
+            
+            guard patternCharacter != replacmentCharacter else { continue }
+            pureNumber.insert(patternCharacter, at: stringIndex)
+        }
+        
+        return pureNumber
+    }
+}
