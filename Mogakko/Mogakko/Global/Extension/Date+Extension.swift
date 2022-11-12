08168a67eef8eeba3e98720c189a5ad9525c8af5
so yeon <alicesysy@swu.ac.kr>
2022-11-12 15:26:18 +0900

    [#16] 회원가입 (생년월일 format) 함수명 수정

--- Mogakko/Mogakko/Global/Extension/Date+Extension.swift
+++ Mogakko/Mogakko/Global/Extension/Date+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Date {
-    func toString() -> String {
+    func toBirthString() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         return dateFormatter.string(from: self)
