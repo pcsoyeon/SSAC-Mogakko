2c9650a67c27dc7ab18bacd80725fe99fbbd54f2
so yeon <alicesysy@swu.ac.kr>
2022-11-13 00:31:23 +0900

    [#19] Timer 메서드 추가

--- Mogakko/Mogakko/Screen/Authorization/ViewModel/CertificationNumberViewModel.swift
+++ Mogakko/Mogakko/Screen/Authorization/ViewModel/CertificationNumberViewModel.swift
@@ -13,6 +13,14 @@ import RxSwift
 
 final class CertificationNumberViewModel {
     
+    // MARK: - Property
+    
+    var timer: Timer!
+    var limitTime = 60
+    
+    var timerString = BehaviorRelay<String>(value: "")
+    var isTimerValid = BehaviorRelay<Bool>(value: true)
+    
     // MARK: - Input, Output
     
     struct Input {
@@ -35,5 +43,31 @@ final class CertificationNumberViewModel {
         return Output(startButtonType: startButtonType,
                       numberTextFieldSignal: numberTextFieldSingal,
                       buttonTap: input.buttonTap)
+    }
+    
+    // MARK: - Helper
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+            self.limitTime -= 1
+            self.updateTimerLabel()
+        })
+    }
+    
+    private func updateTimerLabel() {
+        let minutes = self.limitTime / 60
+        let seconds = self.limitTime % 60
+        
+        if self.limitTime > 0 {
+            timerString.accept(String(format: "%02d:%02d", minutes, seconds))
+        } else {
+            isTimerValid.accept(false)
+            self.timer.invalidate()
+        }
+    }
+    
+    private func stopTimer() {
+        timer.invalidate()
+        limitTime = 60
     }
 }
