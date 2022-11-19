dbed90bb8ff0c4c98dd665fefc38a8d71b2e7207
so yeon <alicesysy@swu.ac.kr>
2022-11-19 15:03:54 +0900

    [#32] Reactive+UISearchBar Extension 추가

--- Mogakko/Mogakko/Global/Extension/Reactive+Extension.swift
+++ Mogakko/Mogakko/Global/Extension/Reactive+Extension.swift
@@ -9,6 +9,8 @@ import UIKit
 
 import RxSwift
 
+// MARK: - Rx+UIViewController
+
 extension Reactive where Base: UIViewController {
     var viewWillAppear: Observable<[Any]> {
         return sentMessage(#selector(UIViewController.viewWillAppear(_:)))
@@ -20,5 +22,15 @@ extension Reactive where Base: UIViewController {
     
     var viewDidLoad: Observable<[Any]> {
         return sentMessage(#selector(UIViewController.viewDidLoad))
+    }
+}
+
+// MARK: - Rx+SearchBar
+
+extension Reactive where Base: UISearchBar {
+    var endEditing: Binder<Void> {
+        return Binder(base) { base, _ in
+            base.endEditing(true)
+        }
     }
 }
